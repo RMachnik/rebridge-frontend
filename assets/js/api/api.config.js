2eda272c1959f76f5b20a4ac620ff0710a21ff2e
rmachnik <rmachnik@yahoo.pl>
2018-09-06 22:12:22 +0200

    Change api config.

--- assets/js/api/api.config.js
+++ assets/js/api/api.config.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'http://vps563480.ovh.net:8080'
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'http://vps563480.ovh.net:8080'
+// export const BASE_URL = 'http://localhost:8080';
 
 export function config(token) {
     return {
